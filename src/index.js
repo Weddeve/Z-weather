85b6715bdc5b0c3ad02896039b7c2066f6a8a01a
Weddeve <169123585+Weddeve@users.noreply.github.com>
2024-05-16 05:21:20 +0200

    undated the html

---
+++ src/index.js
@@ -0,0 +1,9 @@
+This project was coded by
+      <a href="https://github.com/Weddeve" target="_blank"> Tisetso Mankgeru</a>,
+      is
+      <a href="https://github.com/Weddeve/Z-weather" target="_blank"
+        >open-sourced on GitHub</a
+      >
+      and
+      <a href="https://fascinating-marigold-9d09ae.netlify.app/" target="_blank"
+        >hosted on Netlify</a
