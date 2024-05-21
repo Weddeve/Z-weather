e41e489ebcb211cfe97a203624047bd266ee7779
Weddeve <169123585+Weddeve@users.noreply.github.com>
2024-05-21 06:56:55 +0200

    updated html

--- src/index.js
+++ src/index.js
@@ -52,9 +52,32 @@ function handleSearchSubmit(event) {
   searchCity(searchInput.value);
 }
 
+function displayForecast(){
+ let forecast = document.querySelector("#forecast");
+
+ let days = ['Tue','Wed','Thu','Fri','Sat'];
+ let forecastHtml ="";
+
+ days.forEach(function(day){
+  forecastHtml = forecastHtml + `
+<div class="weather-forecast-day">
+        <div class="weather-forecast-date">${day}</div>
+        <div class="weather-forecast-icon">🌤️</div>
+        <div class="weather-forecast-temperatures">
+          <div class="weather-forecast-temperature">
+            <strong>15º</strong>
+          </div>
+          <div class="weather-forecast-temperature">9º</div>
+        </div>
+      </div>`; });
+forecastElement.innerHTML = forecastHtml;
+
+}
+
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
 
 alert("🎉Welcome to Gen-Z first weather platform!");
+displayForecast();
