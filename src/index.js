d873379870e158daea6e6b1c266e2978b49ed2b6
Weddeve <169123585+Weddeve@users.noreply.github.com>
2024-05-24 06:10:11 +0200

    updated

--- src/index.js
+++ src/index.js
@@ -100,3 +100,5 @@ let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
 
 searchCity("Paris");
+
+alert("🎉Welcome to Gen-Z weather")
